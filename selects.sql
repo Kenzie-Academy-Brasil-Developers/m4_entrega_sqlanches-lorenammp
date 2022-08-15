da4ec7dd2bda21adb92eef17a31bd3f8d0e2d052
lorenammp <lorenammpaula@gmail.com>
2022-08-14 21:02:18 -0300

    feat: desenvolvidas tabelas e selects do db

--- selects.sql
+++ selects.sql
@@ -2,11 +2,59 @@
 -- Seleções de dados
 
 -- 1)
+SELECT
+  p.id,
+  p.status,
+  p.cliente_id,
+  prod.id,
+  prod.nome,
+  prod.tipo,
+  prod.preco,
+  prod.pts_de_lealdade
+FROM
+  pedidos p
+  INNER JOIN produtos_pedidos pp ON p.id = pp.pedido_id
+  INNER JOIN produtos prod ON prod.id = pp.produto_id;
 
 -- 2)
+SELECT
+    p.id
+FROM
+  pedidos p
+  INNER JOIN produtos_pedidos pp ON p.id = pp.pedido_id
+  INNER JOIN produtos prod ON prod.id = pp.produto_id
+  WHERE prod.nome LIKE 'Fritas'
 
 -- 3)
+SELECT
+  c.nome
+FROM
+  pedidos p
+  INNER JOIN produtos_pedidos pp ON p.id = pp.pedido_id
+  INNER JOIN produtos prod ON prod.id = pp.produto_id
+  INNER JOIN clientes c ON c.id = p.cliente_id
+  WHERE prod.nome LIKE 'Fritas'
 
 -- 4)
+SELECT
+  SUM(prod.preco)
+FROM
+  pedidos p
+  INNER JOIN produtos_pedidos pp ON p.id = pp.pedido_id
+  INNER JOIN produtos prod ON prod.id = pp.produto_id
+  INNER JOIN clientes c ON c.id = p.cliente_id
+  WHERE c.nome LIKE 'Laura'
+  
+GROUP BY
+  c.nome
 
 -- 5)
+SELECT
+  prod.nome,
+  count(prod.nome)
+FROM
+  pedidos p
+  INNER JOIN produtos_pedidos pp ON p.id = pp.pedido_id
+  INNER JOIN produtos prod ON prod.id = pp.produto_id
+GROUP BY
+  prod.nome
